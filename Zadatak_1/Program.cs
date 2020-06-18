8a6156f25d2854f6787cee6d012639141c845ba9
Kosarevic <kosarevic@gmail.com>
2020-06-18 09:11:35 +0200

    Method Withdraw() added

--- Zadatak_1/Program.cs
+++ Zadatak_1/Program.cs
@@ -10,26 +10,43 @@ namespace Zadatak_1
     class Program
     {
         static readonly Random r = new Random();
+        private static object theLock = new object();
+        static int sum1 = 10000;
+        static int sum2 = 10000;
 
         static void Main(string[] args)
         {
-            int sum = 10000;
-
             Console.WriteLine("Insert number of clients for first bankomat.");
             bool success = int.TryParse(Console.ReadLine(), out int clients);
 
-            if(success)
+            if (success)
             {
                 for (int i = 0; i < clients; i++)
                 {
-                    Thread t = new Thread();
+                    Thread t = new Thread(Withdraw);
+                    t.Start();
                 }
             }
+
+            Console.ReadLine();
         }
 
         static void Withdraw()
         {
+            int withdrawal = r.Next(100, 10000);
 
+            lock (theLock)
+            {
+                if (sum1 - withdrawal >= 0)
+                {
+                    sum1 -= withdrawal;
+                    Console.WriteLine("Remaining sum: " + sum);
+                }
+                else
+                {
+                    Console.WriteLine("Withdrawl sum is larger than sum remaining.");
+                }
+            }
         }
     }
 }
